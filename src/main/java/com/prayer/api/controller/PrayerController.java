c38e2cf1bd2a7e77dcf2008a21bf703e6dde4ab2
school <muhammad.shamaeem@elev.ga.ntig.se>
2024-09-09 21:22:43 +0200

    setup dockerizing

--- src/main/java/com/prayer/api/controller/PrayerController.java
+++ src/main/java/com/prayer/api/controller/PrayerController.java
@@ -4,6 +4,7 @@ import com.prayer.api.model.PrayerIdentifier;
 import com.prayer.api.repository.PrayerIdentifierRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,10 +17,20 @@ import java.util.List;
 public class PrayerController {
 
     private final PrayerIdentifierRepository prayerRepository;
+    private long currentDay = LocalDate.now().getDayOfYear();
 
-    @GetMapping("/43725")
+    @GetMapping("/current")
     public PrayerIdentifier getCurrentPrayer() {
-        long currentDay = LocalDate.now().getDayOfYear();
-        return prayerRepository.findById(1L).get();
+        return prayerRepository.findById(currentDay).get();
+    }
+
+    @GetMapping("/prayer/{prayerId}")
+    public PrayerIdentifier getPrayerById(@PathVariable Long prayerId) {
+        return prayerRepository.findById(prayerId).orElse(prayerRepository.findById(currentDay).get());
+    }
+
+    @GetMapping("/prayers")
+    public List<PrayerIdentifier> getAllPrayers() {
+        return prayerRepository.findAll();
     }
 }
